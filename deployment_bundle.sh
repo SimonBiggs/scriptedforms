5e6bf38752bb1ec98a135fbbda8f427a8ac93ceb
SimonBiggs <mail@simonbiggs.net>
2017-07-07 11:23:11 +1000

    included source within bundle

--- deployment_bundle.sh
+++ deployment_bundle.sh
@@ -5,10 +5,13 @@
 rm -rf build dist
 mkdir -p build/lib
 mkdir -p build/app/python
+mkdir build/src
 
 cp -R ./angular-frontend/dist ./build/app/angular
 cp ./python-server/scriptedforms.py ./build/app/python/scriptedforms.py
 cp -R ./windows_libs/python ./build/lib/python
+
+git add -A; snapshot=`git stash create`; git archive -o build/src/source_code.zip ${snapshot:-HEAD}
 
 wine ./windows_libs/bat2exe.exe -bat ./bootstrap.bat -save ./build/scriptedforms.exe
 
