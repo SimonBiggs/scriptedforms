a75f8607461617976ded974d9c77af43a396dd4a
SimonBiggs <mail@simonbiggs.net>
2017-07-12 11:02:16 +1000

    code is outputting to the markdown form

--- angular/src/app/jupyter/code/code.component.ts
+++ angular/src/app/jupyter/code/code.component.ts
@@ -2,6 +2,10 @@ import {
   Component, OnInit, AfterViewInit, ViewChild, ElementRef
 } from '@angular/core';
 
+import { 
+  RenderMime, defaultRendererFactories
+} from '@jupyterlab/rendermime';
+import { OutputArea, OutputAreaModel } from '@jupyterlab/outputarea';
 import { Kernel } from '@jupyterlab/services';
 
 import { KernelService } from '../kernel.service';
@@ -13,11 +17,18 @@ import { KernelService } from '../kernel.service';
 })
 export class CodeComponent implements OnInit, AfterViewInit {
 
+  renderMimeOptions: RenderMime.IOptions
+  renderMime: RenderMime
+  model: OutputAreaModel
+  outputAreaOptions: OutputArea.IOptions
+  outputArea: OutputArea
+
   promise: Promise<Kernel.IFuture>
   future: Kernel.IFuture
 
   code: string
   @ViewChild('codecontainer') codecontainer: ElementRef
+  @ViewChild('outputcontainer') outputcontainer: ElementRef
 
   constructor(
     private myKernelSevice: KernelService
@@ -28,15 +39,45 @@ export class CodeComponent implements OnInit, AfterViewInit {
 
   ngAfterViewInit() {
     this.code = this.codecontainer.nativeElement.innerHTML
-    // console.log(this.code)
 
+    this.model = new OutputAreaModel()
+
+    this.renderMime = new RenderMime()
+    
+    for (let factory of defaultRendererFactories) {
+      this.renderMime.addFactory(factory)
+    }
+
+    // console.log(this.renderMime)
+
+
+    this.outputAreaOptions = {
+      model: this.model,
+      rendermime: this.renderMime
+    }
+
+    this.outputArea = new OutputArea(this.outputAreaOptions)
+    
     this.promise = this.myKernelSevice.runCode(this.code)
     this.promise.then(future => {
       this.future = future
-      this.future.onIOPub = (msg => {
-        console.log(msg.content)
+      // this.future.onIOPub = (msg => {
+      //   console.log(msg.content)
+      // })
+      this.outputArea.future = this.future
+      // console.log(this.outputArea)
+
+      this.future.done.then(() => {
+        this.outputcontainer.nativeElement.appendChild(this.outputArea.node)
+        // console.log(this.outputcontainer)
       })
     })
+
+
+    // this.outputArea.
+    // this.outputcontainer.nativeElement.appendChild(this.outputArea.node)
+
+
 
   }
 
