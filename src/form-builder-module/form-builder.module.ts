d28cbd81e287c3e0fecee1ecf870347ec5500b9a
SimonBiggs <mail@simonbiggs.net>
2017-12-23 11:43:18 +1100

    update license headewr

--- src/form-builder-module/form-builder.module.ts
+++ src/form-builder-module/form-builder.module.ts
@@ -1,13 +1,13 @@
-// jupyterlab-form
+// scriptedforms
 // Copyright (C) 2017 Simon Biggs
 
 // Licensed under both the Apache License, Version 2.0 (the "Apache-2.0") and 
-// the GNU Affrero General Public License as published by the Free Software 
+// GNU Affrero General Public License as published by the Free Software 
 // Foundation, either version 3 of the License, or (at your option) any later 
 // version (the "AGPL-3.0+").
 
 // You may not use this file except in compiance with both the Apache-2.0 AND 
-// the AGPL-3.0+ in combination.
+// the AGPL-3.0+ in combination (the "Combined Licenses").
 
 // You may obtain a copy of the AGPL-3.0+ at
 
@@ -18,10 +18,10 @@
 //     https://www.apache.org/licenses/LICENSE-2.0.html
 
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the Apache-2.0 and the AGPL-3.0+ is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the Apache-2.0 and the AGPL-3.0+ for the specific language governing 
-// permissions and limitations under the Apache-2.0 and the AGPL-3.0+.
+// distributed under the Combined Licenses is distributed on an "AS IS" BASIS, 
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See 
+// the Combined Licenses for the specific language governing permissions and 
+// limitations under the Combined Licenses.
 
 
 /*
