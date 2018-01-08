e9493129abee9a7e6e0ef6baacf7ab9b4b192600
SimonBiggs <mail@simonbiggs.net>
2018-01-08 23:50:13 +1100

    make component be in router

--- packages/scriptedforms/src/app-routing.module.ts
+++ packages/scriptedforms/src/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
 import { FormBuilderModule } from "./form-builder-module/form-builder.module";
+import { FormBuilderComponent } from "./form-builder-module/form-builder.component";
 
-const appRoutes: Routes = [{ path: "**", component: FormBuilderModule }];
+const appRoutes: Routes = [{ path: "**", component: FormBuilderComponent }];
 
 @NgModule({
-  imports: [RouterModule.forRoot(appRoutes)],
+  imports: [
+    FormBuilderModule,
+    RouterModule.forRoot(appRoutes)
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
