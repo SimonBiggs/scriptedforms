21f0399fd7fa97aa1268c1c9e3c5cff74ea340dd
Simon Biggs <mail@simonbiggs.net>
2018-02-20 14:19:24 +1100

    update all license headers

--- scriptedforms/src/services/session-start-code.ts
+++ scriptedforms/src/services/session-start-code.ts
@@ -1,27 +1,28 @@
-// scriptedforms
+// Scripted Forms -- Making GUIs easy for everyone on your team.
 // Copyright (C) 2017 Simon Biggs
 
-// Licensed under both the Apache License, Version 2.0 (the "Apache-2.0") and 
-// GNU Affrero General Public License as published by the Free Software 
-// Foundation, either version 3 of the License, or (at your option) any later 
-// version (the "AGPL-3.0+").
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version (the "AGPL-3.0+").
 
-// You may not use this file except in compliance with both the Apache-2.0 AND 
-// the AGPL-3.0+ in combination (the "Combined Licenses").
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License and the additional terms for more
+// details.
 
-// You may obtain a copy of the AGPL-3.0+ at
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-//     https://www.gnu.org/licenses/agpl-3.0.txt
+// ADDITIONAL TERMS are also included as allowed by Section 7 of the GNU
+// Affrero General Public License. These aditional terms are Sections 1, 5,
+// 6, 7, 8, and 9 from the Apache License, Version 2.0 (the "Apache-2.0")
+// where all references to the definition "License" are instead defined to
+// mean the AGPL-3.0+.
 
-// You may obtain a copy of the Apache-2.0 at 
-
-//     https://www.apache.org/licenses/LICENSE-2.0.html
-
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the Combined Licenses is distributed on an "AS IS" BASIS, 
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See 
-// the Combined Licenses for the specific language governing permissions and 
-// limitations under the Combined Licenses.
+// You should have received a copy of the Apache-2.0 along with this
+// program. If not, see <http://www.apache.org/licenses/LICENSE-2.0>.
 
 export const sessionStartCode = `
 import json
