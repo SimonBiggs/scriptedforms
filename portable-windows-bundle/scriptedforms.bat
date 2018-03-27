2d5920fa792affd10f68bfd7f4530338f000fb21
Simon Biggs <mail@simonbiggs.net>
2018-03-28 09:17:12 +1100

    Update scriptedforms.bat

--- portable-windows-bundle/scriptedforms.bat
+++ portable-windows-bundle/scriptedforms.bat
@@ -1,1 +1,1 @@
-"%~dp0\python\python-3.5.4.amd64\python.exe" -m scriptedforms "%~dp0\simple-form\simple.md"
+start "%~dp0\python\python-3.5.4.amd64\python.exe" -m scriptedforms "%~dp0\simple-form\simple.md"
