192f32d588d8958086a3182790a5d92b47bf71f3
Simon Biggs <me@simonbiggs.net>
2018-08-18 19:42:13 +1000

    further attempts at sauce labs

--- scriptedforms/e2e/src/protractor.conf.ts
+++ scriptedforms/e2e/src/protractor.conf.ts
@@ -2,19 +2,14 @@ import { Config } from 'protractor';
 
 let config: Config = {
   framework: 'jasmine',
-  capabilities: {
-    browserName: 'chrome'
-  },
   params: {
     token: process.env.JUPYTER_TOKEN
   },
   specs: [ 'utilities/send-token.js', process.env.PROTRACTOR_PATTERN ],
-  seleniumAddress: 'http://localhost:4444/wd/hub',
-
   noGlobals: false
 };
 
-if (process.env.TRAVIS) {
+if (process.env.SAUCE_USERNAME) {
   config.sauceUser = process.env.SAUCE_USERNAME;
   config.sauceKey = process.env.SAUCE_ACCESS_KEY;
   config.capabilities = {
@@ -22,6 +17,11 @@ if (process.env.TRAVIS) {
     'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
     'build': process.env.TRAVIS_BUILD_NUMBER
   };
+} else {
+  config.capabilities = {
+    browserName: 'chrome'
+  }
+  config.seleniumAddress = 'http://localhost:4444/wd/hub'
 }
 
 export { config };
