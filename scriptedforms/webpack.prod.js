70118e78002ff0990522dfa9106ec424d0f76c87
Simon Biggs <me@simonbiggs.net>
2018-08-28 20:38:27 +1000

    comment out uglify in prep for 0.10.0dev0 release

--- scriptedforms/webpack.prod.js
+++ scriptedforms/webpack.prod.js
@@ -5,11 +5,11 @@ var baseConfig = require('./webpack.config.js');
 module.exports = webpackMerge(baseConfig, {
   plugins: [
     new webpack.NoEmitOnErrorsPlugin(),
-    new webpack.optimize.UglifyJsPlugin({
-      mangle: {
-        keep_fnames: true
-      }
-    }),
+    // new webpack.optimize.UglifyJsPlugin({
+    //   mangle: {
+    //     keep_fnames: true
+    //   }
+    // }),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production')
     })
